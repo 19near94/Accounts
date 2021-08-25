657347b7acd26af1a29c6766d3890e14273134ec
JanDuero <jduero@placester.com>
2021-08-25 11:00:20 +0800

    Test

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Accounts.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Accounts.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : Controller
+    {
+        readonly IAccounts _accounts;
+        public AccountController(IAccounts accounts)
+        {
+            _accounts = accounts;
+        }
+        [HttpGet]
+        public IEnumerable<Accounts.Domain.Entities.Accounts> Index()
+        {
+            return _accounts.GetAccounts();
+        }
+    }
+}
